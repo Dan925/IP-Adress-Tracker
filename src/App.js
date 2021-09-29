54ba7a63140024f23ba30b798f65355725a346d9
Dan <dngen029@uottawa.ca>
2021-09-29 19:16:05 -0400

    Ip address tracker app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import Map from "./components/Map";
+import LocationContext from "./components/LocationContext";
+import { useState,useEffect } from "react";
+import axios from "axios";
+
+
 
 function App() {
+  const [ip,setIP] = useState("");
+  const [isp,setISP] = useState("");
+  const [city, setCity] = useState("");
+  const [region,setRegion] = useState("");
+  const [coords,setCoords] = useState([]);
+  const [timez,setTimez]= useState("");
+
+  const updateLocation = (data) => {
+    
+    setISP(data.isp);
+    setCity(data.location.city);
+    setRegion(data.location.region);
+    setCoords([data.location.lat,data.location.lng]);
+    setTimez(data.location.timezone);
+    
+
+  }
+
+  const vals = {
+    ip,isp,city,region,coords,timez,setIP,updateLocation
+  }
+
+  const handleIPChange = (ip) =>{
+    setIP(ip)
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const apiUrl = 'https://geo.ipify.org/api/v1?';
+    const url = apiUrl + 'apiKey=' + apiKey + '&ipAddress=' + ip;
+    axios.get(url).then(res=>{
+        console.log(res.data)
+        updateLocation(res.data);
+        
+
+    });
+  }
+
+  useEffect(()=>{
+    axios.get("https://api.ipify.org/?format=json").then(res=>{
+        
+        handleIPChange(res.data.ip);
+       
+    })
+},[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <LocationContext.Provider value={vals}>
+          <Header handleIPChange={handleIPChange}></Header>
+          <Dashboard></Dashboard>
+          {coords.length>0 && 
+           <Map></Map>
+          }
+         
+        </LocationContext.Provider>
+        
+      </div>
     </div>
   );
 }
